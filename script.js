e0f338db93e24c27e47efde421c21796323b8520
Janice R <rodriguezjaniece@gmail.com>
2022-03-26 21:12:07 -0400

    added array, global var and for loop

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 // Start your code ***HERE*** =========
 
-// create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
+function addNewPassword() {
 
-// create a global variable called "pwLength" with a number between 10 and 18
+    // create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
+    const characterType = [['~', '`', '!', '@', '#','$','%','^','&','*',(','),'-','_','+','=','?'], 
+    ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+    ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+    [0,1,2,3,4,5,6,7,8,9]];
+    
+    // create a global variable called "pwLength" with a number between 10 and 18
+    const pwLength = Math.floor(Math.random() * 9) + 10;
+    let randomPassword = [];
 
-// Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
+    // Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
+   for (i = 0; i < pwLength; i++) {
+       let randomIndex = 
+         characterType[Math.floor(Math.random() * characterType.length)];
+       let randomSelector = Math.floor(Math.random() * randomIndex.length);
+       randomPassword.push(randomIndex[randomSelector]);
+   }
+   console.log(randomPassword.join(''));
+   return randomPassword.join('');
+    
+}
+
+
+
 
 // ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
 
@@ -16,3 +37,6 @@ let buttonHandler = () => {
 genBtn.addEventListener("click", buttonHandler);
 
 // ========= ⬆ DO NOT TOUCH THIS CODE ⬆ ======
+
+
+
